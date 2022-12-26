932bb433d8aab885a4029cfcc8e6ffe247733dad
georgetmoore <mooretgeorge@gmail.com>
2022-12-26 16:14:58 +0000

    Initial setup

---
+++ src/CalorieCounting/CalorieCounting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(textFile);
+            Console.WriteLine(lines[0]);
+            Console.ReadLine();
+        }
+    }
+}
