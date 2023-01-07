4446cf59521f2f52968cc4be20133e0d220f8898
mooregt <g.t.moore@lancaster.ac.uk>
2023-01-07 15:33:16 +0000

    Added ctor for calories repository

--- src/CalorieCounting/CalorieCounting/CaloriesRepository.cs
+++ src/CalorieCounting/CalorieCounting/CaloriesRepository.cs
@@ -4,6 +4,8 @@ namespace CalorieCounting
 {
     public class CaloriesRepository
     {
+        public CaloriesRepository() { }
+
         public string[] GetCaloriesInput()
         {
             string[] lines = File.ReadAllLines("../../../../../libs/input.txt");
